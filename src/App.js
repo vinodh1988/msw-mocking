6b8e162645a8a12005fb3574bc0b3c79a2130d40
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-09-21 18:11:14 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import {useState,useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+    let [first,setFirst]=useState("")
+    let [last,setLast]=useState("")
+
+    useEffect(()=>{
+      let fetchData= async ()=>{
+         let response = await axios.get("https://randomuser.me/api/")
+         setFirst(response.data.results[0].name.first)
+         setLast(response.data.results[0].name.last)
+      }
+      fetchData()
+  },[])
+
+   if(first==="")
+      return (<h3>Loading....</h3>)
+
+
+    return(
+       <div>
+         <form>
+           First Name<input type="text"
+              aria-label="first"
+              value={first}
+              onChange={(e)=>setFirst(e.target.value)}
+            /> <br/>
+            Last Name<input type="text"
+              value={last}
+              aria-label="last"
+              onChange={(e)=>setLast(e.target.value)}
+            /><br/>
+         </form>
+         <h2 >
+           FirstName: {first}, LastName: {last}
+         </h2>
+       </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen,fireEvent } from '@testing-library/react';
 import App from './App';
+import {setupServer} from 'msw/node'
+import {rest} from 'msw'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const server=setupServer(
+  rest.get('https://randomuser.me/api/',(req,res,ctx)=>{
+     return res(
+           ctx.json(
+             {
+               results:[
+                 {
+                   name:{
+                     first:"Peter",
+                     last:"Johnson"
+                   }
+                 }
+               ]
+             }
+           )
+     )
+  })
+)
+
+describe("Api Component Testing",()=>{
+    beforeAll(()=>server.listen())
+    afterEach(()=>server.resetHandlers())
+    afterAll(()=>server.close())
+
+    test("component should be loaded properly",async ()=>{
+       render(<App/>)
+       let result=await screen.findAllByText(/Peter/i);
+       expect(result[0]).toHaveTextContent(/Peter/i)
+       expect(result[0]).toHaveTextContent(/Johnson/i)
+    })
+
+    test('text box should impact target container',async ()=>{
+      render(<App/>)
+      const element=await screen.findByLabelText(/first/i)
+      fireEvent.change(element,{target:{value:'Nicholas'}})
+      const testElement=screen.getByRole("heading")
+      expect(testElement).toHaveTextContent(/Nicholas/i)
+    })
+})
